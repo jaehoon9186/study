e977d1a76900ced4c864eb6673a22eb5820ade89
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-02-24 17:43:24 +0900

    [Bronze III] Title: 곱셈, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.swift
+++ 백준/Bronze/2588. 곱셈/곱셈.swift
@@ -2,14 +2,9 @@ import Foundation
 
 let input1 = Int(readLine()!)!
 let input2 = readLine()!
+let input2St = input2.map { Int(String($0))! }
 
-print(
-    input1 * Int(String(input2[input2.index(input2.startIndex, offsetBy: 2)]))!
-)
-print(
-    input1 * Int(String(input2[input2.index(input2.startIndex, offsetBy: 1)]))!
-)
-print(
-    input1 * Int(String(input2[input2.index(input2.startIndex, offsetBy: 0)]))!
-)
+print(input1 * input2St[2])
+print(input1 * input2St[1])
+print(input1 * input2St[0])
 print(input1 * Int(input2)!)
