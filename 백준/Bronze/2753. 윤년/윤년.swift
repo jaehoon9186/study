2a5fd97c4fbedebf0aba3ad7629dc979742c65c9
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-02-24 19:13:05 +0900

    [Bronze V] Title: 윤년, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.swift
+++ 백준/Bronze/2753. 윤년/윤년.swift
@@ -1,3 +1,7 @@
 let input = Int(readLine()!)!
 
-print(input % 400 == 0 ? "1" : input % 4 == 0 && input % 100 != 0 ? "1" : "0")
+if (input % 400 == 0) || (input % 4 == 0 && input % 100 != 0) {
+    print("1")
+} else {
+    print("0")
+}
