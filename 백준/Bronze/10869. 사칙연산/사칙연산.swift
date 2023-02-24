a837a07cdf3522ac419e09b0536ebd0885e988e4
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-02-24 15:44:30 +0900

    [Bronze V] Title: 사칙연산, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+
+print(input[0] + input[1])
+print(input[0] - input[1])
+print(input[0] * input[1])
+print(input[0] / input[1])
+print(input[0] % input[1])
