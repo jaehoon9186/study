de5b1934e5cd74d180fe391a39b4fa3416133418
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-03-16 18:13:43 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.swift
@@ -0,0 +1,30 @@
+let inputArray = Array(readLine()!)
+let croatiaAlphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+var count = 0
+var idx = 0
+
+while idx < inputArray.count {
+
+    var flag = 0
+
+    // 특수문자
+    for i in (2...3) {
+        if idx + i <= inputArray.count {
+            let sub = inputArray[idx..<idx + i].map { String($0) }.joined()
+            if croatiaAlphabet.contains(sub) {
+                idx += i
+                count += 1
+                flag = 1
+                break
+            }
+        }
+    }
+
+    // 일반문자
+    if flag == 0 {
+        idx += 1
+        count += 1
+    }
+}
+
+print(count)
