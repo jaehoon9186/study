f7eee8d25be929771b44a0174fe405eecaaf4631
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-04-05 21:59:06 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.swift
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.swift
@@ -1,10 +1,4 @@
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let (x, y, w, h) = (input[0], input[1], input[2], input[3])
 
-var answer = x
-
-answer = min(answer, y)
-answer = min(answer, w - x)
-answer = min(answer, h - y)
-
-print(answer)
+print(min(x, y, w - x, h - y))
