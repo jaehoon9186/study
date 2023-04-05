5a23a066bc0ec484cd7a6ead91e2085fbc879048
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-04-05 12:21:20 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.swift
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-let (a, b, v) = (Double(input[0]), Double(input[1]), Double(input[2]))
+let input = readLine()!.split(separator: " ").map { Double($0)! }
+let (a, b, v) = (input[0], input[1], input[2])
 
 var n = ceil((v-a)/(a-b))
 
