7ff3c9ee9c826a3900e05e6899565efb80471f4d
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-04-21 17:34:33 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 308 ms, Memory: 73540 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.swift
@@ -0,0 +1,19 @@
+let input = Int(readLine()!)!
+var dotList: [(Int, Int)] = []
+
+(0..<input).forEach { _ in
+    let temp = readLine()!.split(separator: " ").map { Int($0)! }
+    let xy = (temp[0], temp[1])
+    dotList.append(xy)
+}
+
+dotList = dotList.sorted(by: { (first, second) -> Bool in
+    if first.0 == second.0 {
+        return first.1 < second.1
+    }
+    return first.0 < second.0
+})
+
+for i in dotList {
+    print(i.0, i.1)
+}
