e73d734266a836d03c6c125f47e2ea3da10f18cd
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-04-21 15:11:05 +0900

    [Bronze II] Title: 수 정렬하기, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.swift
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.swift
@@ -1,11 +1,27 @@
 var n = Int(readLine()!)!
 var list: [Int] = []
 
-(0..<n).forEach { _ in
-    list.append(Int(readLine()!)!)
+for _ in (0..<n) {
+    let input = Int(readLine()!)!
+
+    if list.isEmpty {
+        list.append(input)
+        continue
+    }
+
+    for i in list.indices {
+        if input < list[i] {
+            list.insert(input, at: i)
+            break
+        }
+
+        if i == list.count - 1 {
+            list.append(input)
+        }
+    }
+
 }
 
-list.sort()
 
 for i in list {
     print(i)
