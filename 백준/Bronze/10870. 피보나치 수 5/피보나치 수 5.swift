c39bd3b3f10d2c8d1768578ce89e1cf1002c7ed5
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-05-16 14:06:15 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.swift
@@ -0,0 +1,14 @@
+func fibonacci(_ n: Int) -> Int {
+    if n <= 0 {
+        return 0
+    }
+
+    if n == 1 {
+        return 1
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+}
+
+print(fibonacci(Int(readLine()!)!))
