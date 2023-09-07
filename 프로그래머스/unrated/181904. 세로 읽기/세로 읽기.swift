2afd64387209f0b1bcd847894c91eb7b766b476c
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-09-07 17:59:48 +0900

    [unrated] Title: 세로 읽기, Time: 0.09 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+func solution(_ my_string:String, _ m:Int, _ c:Int) -> String {
+    return stride(from: 0, to: my_string.count, by: m).map { String(my_string[my_string.index(my_string.startIndex, offsetBy: $0 + c - 1)]) }.joined()
+}
