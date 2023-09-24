77c63548113aa7c30a7bc8c9b7c27cbdf7534c06
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-09-24 20:11:03 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.45 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.swift
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.swift
@@ -2,19 +2,7 @@ import Foundation
 
 func solution(_ s:String) -> Int {
     
-    let sList = s.split(separator: " ").map { String($0) }
+    let newS = s.replacingOccurrences(of: " Z", with: "Z")
     
-    var result = 0
-    
-    for i in 0..<sList.count {
-        if let a = Int(sList[i]) {
-            if i == sList.count - 1 || sList[i + 1] != "Z" {
-                result += a
-            }
-        }
-    }
-    
-    
-    
-    return result
+    return newS.split(separator: " ").map { Int($0) ?? 0 }.reduce(0, +)
 }
