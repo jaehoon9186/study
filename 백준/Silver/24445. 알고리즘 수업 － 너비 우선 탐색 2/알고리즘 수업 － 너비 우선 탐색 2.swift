d39c5908b2287acd3db96c5989e00033411c15bf
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2023-11-23 06:09:56 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 380 ms, Memory: 82096 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.swift
@@ -0,0 +1,45 @@
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let (n, m, startNode) = (input[0], input[1], input[2])
+
+var graph = Array(repeating: [Int](), count: n + 1)
+var visited = Array(repeating: false, count: n + 1)
+var visitedDepth = Array(repeating: 0, count: n + 1)
+var queue: [Int] = []
+
+for _ in 0..<m {
+    let temp = readLine()!.split(separator: " ").map { Int($0)! }
+    let (u, v) = (temp[0], temp[1])
+    graph[u].append(v)
+    graph[v].append(u)
+}
+
+
+var depth = 0
+
+func bfs(_ node: Int) {
+
+    visited[node] = true
+    queue.append(node)
+
+    var index = 0
+
+    while index < queue.count {
+        let nowNode = queue[index]
+        depth += 1
+        visitedDepth[nowNode] = depth
+
+        for i in graph[nowNode].sorted(by: >) {
+            if visited[i] == false {
+                visited[i] = true
+                queue.append(i)
+            }
+        }
+
+        index += 1
+    }
+
+}
+
+bfs(startNode)
+
+print(visitedDepth[1...].map { String($0) }.joined(separator: "\n"))
